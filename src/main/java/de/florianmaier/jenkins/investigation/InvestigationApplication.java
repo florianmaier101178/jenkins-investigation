304fec037bf956e6e442f58bd4fecbe0927683d1
Florian Maier <florianmaier101178@gmail.com>
2018-07-30 21:24:30 +0200

    project setup

---
+++ src/main/java/de/florianmaier/jenkins/investigation/InvestigationApplication.java
@@ -0,0 +1,12 @@
+package de.florianmaier.jenkins.investigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestigationApplication.class, args);
+	}
+}
